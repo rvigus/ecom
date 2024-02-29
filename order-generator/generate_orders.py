5605e85e684f780f5bc9113a8741d4f8b95aa14f
rvigus <roryvigus@gmail.com>
2024-02-29 20:55:03 +0000

    Adding GCS

--- order-generator/generate_orders.py
+++ order-generator/generate_orders.py
@@ -1,37 +1,33 @@
 import json
 import random
+import os
 from datetime import datetime
 from time import sleep
 
-import boto3
+from google.cloud import storage
 import pandas as pd
 from faker import Faker
 
 # Init faker
 fake = Faker()
 
-
-# Configure S3
-s3 = boto3.client('s3',
-                  endpoint_url='http://localstack:4566',
-                  aws_access_key_id='test',
-                  aws_secret_access_key='test',
-                  region_name='us-east-1')
-
-bucket_name = 'data'
+# Configure GCS
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/usr/src/app/secrets/gcp-secrets.json"
+client = storage.Client()
+bucket_name = 'ecom-data-1051-11'
 prefix = "orders"
 
-def load_product_catalog():
+def load_product_catalog() -> pd.DataFrame:
     return pd.read_csv("./data/skus.csv", sep=';')
 
-def get_discount_rate(lower, upper, multiple):
+def get_discount_rate(lower: float, upper: float, multiple: float) -> float:
     random_int = random.randint(lower, int(upper / multiple))
     return random_int * multiple
 
-def get_discount_value(price, quantity, discount_rate):
+def get_discount_value(price: float, quantity: float, discount_rate: float) -> float:
     return price * quantity * discount_rate
 
-def get_payment_method():
+def get_payment_method() -> str:
     method = [
         "PayPal",
         "Visa",
@@ -44,7 +40,7 @@ def get_payment_method():
     ]
     return random.choice(method)
 
-def get_shipping_method():
+def get_shipping_method() -> str:
     shipping = [
         "standard",
         "express",
@@ -53,7 +49,12 @@ def get_shipping_method():
     return random.choices(shipping, weights=[0.80, 0.15, 0.05], k=1)[0]
 
 
-def generate_order(seed_df):
+def generate_order(seed_df: pd.DataFrame) -> str:
+    """
+    Generate a random order
+    :param seed_df: Our product catalog dataframe
+    :return: json string
+    """
 
     sku_id = random.choice(seed_df['sku_id'].to_list())
     price = seed_df[seed_df['sku_id'] == sku_id]['price'].iat[0]
@@ -81,21 +82,26 @@ def generate_order(seed_df):
     }
     return json.dumps(order)
 
-def upload_to_s3(order_data):
-
+def upload_to_gcs(order_data: str) -> None:
+    """
+    Upload order data to GCS
+    :param order_data: str
+    :return: None
+    """
     j = json.loads(order_data)
     order_id = j['order_id']
     order_dt = j["order_ts"][:10]
 
-    s3_path = f"{prefix}/dt={order_dt}/order_{order_id}.json"
-    s3.put_object(Bucket=bucket_name, Key=s3_path, Body=order_data)
-    print(f"Uploaded order {order_id} to S3 bucket {bucket_name}/{s3_path}.")
+    blob_path = f"{prefix}/dt={order_dt}/order_{order_id}.json"
+    blob = client.bucket(bucket_name).blob(blob_path)
+    blob.upload_from_string(order_data)
+    print(f"Uploaded order {order_id} to GCS bucket {bucket_name}/{blob_path}.")
 
 
 if __name__ == "__main__":
     skus = load_product_catalog()
     while True:
         order_data = generate_order(skus)
-        upload_to_s3(order_data)
+        upload_to_gcs(order_data)
         print(order_data, flush=True)
         sleep(random.randint(1, 10))
